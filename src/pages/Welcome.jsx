0f20f5c1a047b0e9a47a864516f713b13ed01ba5
Amber Roberts <a.roberts_90@yahoo.com>
2024-12-16 10:55:35 -0600

    added router files

---
+++ src/pages/Welcome.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Welcome () {
+    return (
+        <>
+        <h1>Pokemon are the best!</h1>
+        <p>Try searching by name or id in the Gotta catch em all section, or just scroll on Find a Pokemon if you please!</p>
+        </>
+    )
+}
+
+export default Welcome;
